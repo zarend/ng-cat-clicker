83660e0e7a52d7c73467c91d9ba2b13d58f3dbe5
Zach Arend <zachzach@google.com>
2020-12-14 14:40:32 -0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CatComponent } from './cat/cat.component';
+import { CatListComponent } from './cat-list/cat-list.component';
+import { CoolNamePipe } from './cool-name.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatListModule} from '@angular/material/list'
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatComponent,
+    CatListComponent,
+    CoolNamePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
